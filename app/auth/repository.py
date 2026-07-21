3d5a5804bc76f5222e13a5cf119d406cf201155a
Rhys Malyon <malyon@housingjapan.com>
2026-07-21 16:29:21 +0900

    feat: set up auth repository

--- app/auth/repository.py
+++ app/auth/repository.py
@@ -1,6 +1,16 @@
+import logging
+
+import aiomysql
 from aiomysql import Connection
+from pypika import functions as fn, FormatParameter, Table
+from pypika.dialects import MySQLQuery
 
 from app.auth.schemas import InsertTokenPayload, RefreshTokenPayload, User
+
+logger = logging.getLogger(__name__)
+
+refresh_tokens_table = Table("refresh_tokens", schema="e-smile")
+users_table = Table("users", schema="e-smile")
 
 
 # LOGIN
@@ -9,15 +19,62 @@ class UserNotFoundError(Exception):
 
 
 async def find_user_by_email(conn: Connection, email: str) -> User:
-    pass
+    async with conn.cursor(aiomysql.DictCursor) as cur:
+        query = (
+            MySQLQuery.from_(users_table)
+            .select(users_table.star)
+            .where(users_table.email == FormatParameter())
+        )
+
+        sql_string = query.get_sql()
+
+        logger.info(sql_string)
+
+        await cur.execute(sql_string, (email,))
+        result = await cur.fetchone()
+
+        if result is None:
+            raise UserNotFoundError("Could not find user.")
+
+        return User(**result)
 
 
 async def update_last_accessed(conn: Connection, user_id: int) -> None:
-    pass
+    async with conn.cursor() as cur:
+        query = (
+            MySQLQuery.update(users_table)
+            .set("last_accessed", fn.Now())
+            .where(users_table.id == FormatParameter())
+        )
+
+        sql_string = query.get_sql()
+
+        logger.info(sql_string)
+
+        await cur.execute(sql_string, (user_id,))
 
 
 async def insert_refresh_token(conn: Connection, token_payload: InsertTokenPayload) -> None:
-    pass
+    async with conn.cursor() as cur:
+        query = (
+            MySQLQuery.into(refresh_tokens_table)
+            .columns("token_hash", "session_id", "user_id", "expires_at")
+            .insert(FormatParameter(), FormatParameter(), FormatParameter(), FormatParameter())
+        )
+
+        sql_string = query.get_sql()
+
+        logger.info(sql_string)
+
+        await cur.execute(
+            sql_string,
+            (
+                token_payload.token_hash,
+                token_payload.session_id,
+                token_payload.user_id,
+                token_payload.expires_at,
+            ),
+        )
 
 
 # REFRESH
@@ -28,17 +85,51 @@ class RefreshTokenNotFoundError(Exception):
 async def find_hashed_refresh_token(
     conn: Connection, refresh_token_hash: str
 ) -> RefreshTokenPayload:
-    pass
+    async with conn.cursor(aiomysql.DictCursor) as cur:
+        query = (
+            MySQLQuery.from_(refresh_tokens_table)
+            .select("user_id", "session_id", "is_valid")
+            .where(users_table.token_hash == FormatParameter())
+        )
 
+        sql_string = query.get_sql()
 
-async def rotate_refresh_token(conn: Connection, refresh_token_hash: str) -> None:
-    pass
+        logger.info(sql_string)
+
+        await cur.execute(sql_string, (refresh_token_hash,))
+        result = await cur.fetchone()
+
+        if result is None:
+            raise RefreshTokenNotFoundError("Could not find refresh token.")
+
+        return RefreshTokenPayload(**result)
 
 
 async def invalidate_session_tokens(conn: Connection, session_id: str) -> None:
-    pass
+    async with conn.cursor() as cur:
+        query = (
+            MySQLQuery.update(refresh_tokens_table)
+            .set("is_valid", 0)
+            .where(refresh_tokens_table.session_id == FormatParameter())
+        )
+
+        sql_string = query.get_sql()
+
+        logger.info(sql_string)
+
+        await cur.execute(sql_string, (session_id,))
 
 
-# LOGOUT
-async def invalidate_refresh_token_hash(conn: Connection, refresh_token_hash: str) -> None:
-    pass
+async def invalidate_refresh_token(conn: Connection, refresh_token_hash: str) -> None:
+    async with conn.cursor() as cur:
+        query = (
+            MySQLQuery.update(refresh_tokens_table)
+            .set("is_valid", 0)
+            .where(refresh_tokens_table.token_hash == FormatParameter())
+        )
+
+        sql_string = query.get_sql()
+
+        logger.info(sql_string)
+
+        await cur.execute(sql_string, (refresh_token_hash,))
